d141f227651cebfec858929810969b88d0d69cb1
vergilhui <vergilhui@gmail.com>
2015-12-30 11:04:12 +0800

    modify user space program android log modify kernel module, add dial record and remove error netfilter hook

--- module/module_main.c
+++ module/module_main.c
@@ -6,6 +6,10 @@
 #include <net/sock.h>
 #include <net/netlink.h>
 #include <linux/skbuff.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/netdevice.h>
+#include <linux/ip.h>
 #include <linux/string.h>
 
 MODULE_LICENSE("GPL");
@@ -14,74 +18,54 @@ MODULE_DESCRIPTION("Android App Behavior Record");
 
 //static void **sys_call_table;
 #define SYS_CALL_TB 0xc000da84
-#define SMS "AT+CMGS"
 #define MY_GROUP 1
 #define MAX_SIZE 1024
-#define MODEL "AT+CMGF"
+#define SMS "AT+CMGS"
 #define IMEI "AT+CGSN"
-#define DIAL "ATD"
+#define CALL "ATD"
 #define HANGUP "ATH"
 #define AWCALL "ATA"
 #define AUTOAW "ATS0"
+#define SHANGUP "AT+CHUP"
+#define NIPQUAD(addr) \
+	((unsigned char *)&addr)[0], \
+	((unsigned char *)&addr)[1], \
+	((unsigned char *)&addr)[2], \
+	((unsigned char *)&addr)[3]
 
 unsigned long *sys_call_table = (unsigned long *)SYS_CALL_TB;
 int (*orig_write)(unsigned int fd, char *buf, unsigned int count);
 int (*orig_open)(const char *file, int flag, mode_t mode);
-unsigned long i_sms_len = 0;
-unsigned int sms_model = 0;
-char textno[32] = {0};
+unsigned long i_sms_len = -10;
 
 struct task_struct *mythread = NULL;
 struct sock *nl_sk = NULL;
-
-static struct nf_hook_ops out_nfho;
-
-static int init_nfcheck(void)
-{
-	out_nfho.hook = nfhook;
-	out_nfho.owner = THIS_MODULE;
-	out_nfho.hooknum = NF_IP_LOCAL_OUT;
-	out_nfho.pf = PF_INET;
-	out_nfho.priority = NF_IP_PRI_FIRST;
-	nf_register_hook(&out_nfho);
-	return 0;
-}
-
-unsigned int nfhook(
-	unsigned int hooknum,
-	struct sk_buff *skb,
-	const struct net_device *in,
-	const struct net_device *out,
-	int (*okfn)(struct sk_buff *))
-{
-	struct iphdr *iph;
-
-	iph = (struct iphdr *)skb_network_header(skb);
-	char msg[128] = {0};
-	snprintf(msg, 128,
-		"{'netObject':['saddr':'%d', 'daddr':'%d', 'protocol':'%d']}\n",
-		iph->saddr, iph->daddr, iph->protocol);
-	kernel_send_nl_msg(strim(msg));
- 	printk("network!");
-}
+struct sk_buff *socket_buff = NULL;
 
 static void nl_receive_callback (struct sk_buff *skb)
 {
+	printk("Free sk_buff!\n");
     nlmsg_free(skb);
 }
 
 static void kernel_send_nl_msg(const char *msg)
 {
     struct nlmsghdr *nlsk_mh;
-    struct sk_buff* socket_buff;
+    //struct sk_buff *socket_buff;
     
     socket_buff = nlmsg_new(MAX_SIZE, GFP_KERNEL);
+    if (!socket_buff)
+    	printk("socket_buff NULL\n");
 
     nlsk_mh = nlmsg_put(socket_buff, 0, 0, NLMSG_DONE, MAX_SIZE, 0);
+    if (!nlsk_mh)
+    	printk("nlsk_mh NULL\n");
 
     NETLINK_CB(socket_buff).pid = 0;
     NETLINK_CB(socket_buff).dst_group = MY_GROUP;
 
+    if (!msg)
+    	printk("msg NULL \n");
     strcpy(nlmsg_data(nlsk_mh), msg);
 
     nlmsg_multicast(nl_sk, socket_buff, 0, MY_GROUP, GFP_KERNEL);
@@ -89,79 +73,112 @@ static void kernel_send_nl_msg(const char *msg)
     return;
 }
 
+/*unsigned int nfhook(
+	unsigned int hooknum,
+	struct sk_buff *skb,
+	const struct net_device *in,
+	const struct net_device *out,
+	int (*okfn)(struct sk_buff *))
+{
+	struct iphdr *iph;
+	char inetmsg[128] = {0};
+	//struct udphdr *udph;
+	//struct tcphdr *tcph;
+	__be32 sip, dip;
+
+	if (skb)
+	{
+		iph = ip_hdr(skb);
+		if (iph	&& (iph->protocol == IPPROTO_UDP || iph->protocol == IPPROTO_TCP))
+		{
+			sip = iph->saddr;
+			dip = iph->daddr;
+			
+			snprintf(inetmsg, 128,
+				"{'netObject':[{'saddr':'%d.%d.%d.%d:%u', 'daddr':'%d.%d.%d.%d:%u', 'protocol':'%x'}]}\n",
+				NIPQUAD(sip), NIPQUAD(dip), iph->protocol);
+			//kernel_send_nl_msg(strim(inetmsg));
+ 			printk("%s\n", inetmsg);
+		}
+		
+	}
+	
+ 	return NF_ACCEPT;
+}
+
+struct nf_hook_ops out_nfho = {
+	.list = {NULL, NULL},
+	.hook = nfhook,
+	//.hooknum = NF_INET_POST_ROUTING,
+	.hooknum = NF_INET_FORWARD,
+	.pf = PF_INET,
+	.priority = NF_IP_PRI_FIRST,
+};*/
+
 int new_open(const char *file, int flag, mode_t mode)
 {
 	char *contact = "/data/data/com.android.providers.contacts/databases/contacts2.db";
   	char *telephony = "/data/data/com.android.providers.telephony/databases/telephony.db";
   	char *sms = "/data/data/com.android.providers.telephony/databases/mmssms.db";
+  	char *download = "/data/data/com.android.providers.downloads/databases/downloads.db";
   	char *apk = ".apk";
   	unsigned type = -1;
   	if ((type = (strcmp(file, contact) == 0) ? 1:0)
   		|| (type = (strcmp(file, telephony) == 0) ? 2:0)
-  		|| (type = (strcmp(file, sms) == 0) ? 3:0
-  		|| (strstr(s1, s2) && type = (strlen(strstr(s1, s2)) == strlen(s2) ? 4:0))
+  		|| (type = (strcmp(file, sms) == 0) ? 3:0)
+  		|| (type = (strcmp(file, download) == 0) ? 4:0)
+  		|| (strstr(file, apk) && (type = (strlen(strstr(file, apk)) == strlen(apk) ? 5:0))))
   	{
   		char msg[128] = {0};
-		snprintf(msg, 128, "{'pvcObject':['path':'%s', 'type':'%d', action':'open']}\n", buf, type);
+		snprintf(msg, 128, "{'pvcObject':[{'path':'%s', 'type':'%d', action':'open'}]}\n", file, type);
 		kernel_send_nl_msg(strim(msg));
  		printk("Open privacy file! Path: %s!!!\n", file);
 	}
+	return orig_open(file, flag, mode);
 }
 
 int new_write(unsigned int fd, char *buf, unsigned int count)
 {
-	if (sms_model == 0 && (i_sms_len * 2 + 2) == strlen(buf))
+	if ((i_sms_len * 2 + 2) == strlen(buf))
 	{
 		char msg[512] = {0};
-		snprintf(msg, 512, "{'smsObject':['len':'%ld', 'sms':'%s', 'model': '0']}\n", i_sms_len, buf);
+		snprintf(msg, 512, "{'smsObject':[{'len':'%ld', 'sms':'%s', 'model':'0'}]}\n", i_sms_len, buf);
 		printk(msg);
 		kernel_send_nl_msg(strim(msg));
-		i_sms_len = 0;
+		i_sms_len = -10;
 	}
-	if (strstr(buf, MODEL))
+	if (strstr(buf, SMS) && !strstr(buf, "\""))
 	{
-		char c_sms[127] = {0};
+		char c_sms[128] = {0};
 		char *p_sms = c_sms;
 		strcpy(p_sms, buf);
 		strsep(&p_sms, "=");
-		kstrtol(p_sms, 0, &sms_model);
-	}
-	if (strstr(buf, SMS))
-	{
-		char c_sms[127] = {0};
-		char *p_sms = c_sms;
-		strcpy(p_sms, buf);
-		strsep(&p_sms, "=");
-		if (sms_model == 0)
-		{
-			kstrtol(p_sms, 0, &i_sms_len);
-			//printk("AT Command->%s\n", buf);
-		}
-		else
-		{
-			strncpy(textno, p_sms, 32);
-			char msg[512] = {0};
-			snprintf(msg, 512, "{'smsObject':['len':'%d', 'sms':'%s', 'model': '1']}\n", strlen(buf), buf);
-			printk(msg);
-			kernel_send_nl_msg(strim(msg));
-		}
+		kstrtol(p_sms, 0, &i_sms_len);
+		//printk("AT Command->%s\n", buf);
 	}
 	if (strstr(buf, IMEI))
 	{
-		char msg[32] = {0};
-		snprintf(msg, 32, "{'imeiObject':['cmd':'%s']}\n", buf);
-		printk(msg);
-		kernel_send_nl_msg(strim(msg));
+		char imeimsg[32] = {0};
+		snprintf(imeimsg, 32, "{'imeiObject':[{'cmd':'%s'}]}\n", buf);
+		printk(imeimsg);
+		kernel_send_nl_msg(strim(imeimsg));
 	}
-	if (strstr(buf, DIAL)
-		|| strstr(buf, HANGUP) 
-		|| strstr(buf, AWCALL)
+	if (strstr(buf, CALL))
+	{
+		char callmsg[64] = {0};
+		snprintf(callmsg, 64, "{'callObject':[{'cmd':'%s'}]}\n", buf);
+		printk(callmsg);
+		kernel_send_nl_msg(strim(callmsg));
+	}
+	if ((strstr(buf, HANGUP) && strlen(strstr(buf, HANGUP)) == strlen(HANGUP))
+		|| (strstr(buf, SHANGUP) && strlen(strstr(buf, SHANGUP)) == strlen(SHANGUP))
+		|| (strstr(buf, AWCALL) && strlen(strstr(buf, AWCALL)) == strlen(AWCALL))
 		|| strstr(buf, AUTOAW))
 	{
-		char msg[32] = {0};
-		snprintf(msg, 32, "{'dialObject':['cmd':'%s']}\n", buf);
-		printk(msg);
-		kernel_send_nl_msg(strim(msg));
+		char dialmsg[32] = {0};
+		snprintf(dialmsg, 32, "{'dialObject':[{'cmd':'%s'}]}\n", buf);
+		printk(dialmsg);
+		kernel_send_nl_msg(strim(dialmsg));
 	}
 	
 	return orig_write(fd, buf, count);
@@ -171,7 +188,9 @@ int init_module(void)
 {
 	//get_sys_call_table();
 	orig_write = sys_call_table[__NR_write];
+	orig_open = sys_call_table[__NR_open];
 	sys_call_table[__NR_write] = &new_write;
+	sys_call_table[__NR_open] = &new_open;
 	nl_sk = netlink_kernel_create(&init_net, NETLINK_USERSOCK, 0, nl_receive_callback, NULL, THIS_MODULE);
 	if(!nl_sk)
 	{
@@ -180,16 +199,19 @@ int init_module(void)
     }
 	mythread = kthread_run(kernel_send_nl_msg, "init", "kernel_monitor");
 	printk("module_write module ready!");
+	//nf_register_hook(&out_nfho);
 	return 0;
 }
 
 void cleanup_module(void)
 {
 	sys_call_table[__NR_write] = orig_write;
+	sys_call_table[__NR_open] = orig_open;
+	
 	if(nl_sk != NULL)
 	{
         sock_release(nl_sk->sk_socket);
 	}
-	nf_unregister_hook(&out_nfho);
+	//nf_unregister_hook(&out_nfho);
 	printk("remove module success!");
 }
--- usr_netlink/jni/usr_netlink.c
+++ usr_netlink/jni/usr_netlink.c
@@ -2,10 +2,13 @@
 #include <linux/netlink.h>
 #include <stdio.h>
 #include <errno.h>
+#include <android/log.h>
 
 #define NETLINK_TEST 17
 #define MY_GROUP 1
 #define MAX_SIZE 1024
+#define LOG_TAG "AppanalyTag"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 int main(int argc, char* argv[])
 {
@@ -14,12 +17,16 @@ int main(int argc, char* argv[])
     struct nlmsghdr *nl_msghdr;
     struct msghdr msghdr;
     struct iovec iov;
-    char* kernel_msg;
+    char error_msg[256] = {0};
+    char *kernel_msg;
 
     sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USERSOCK);
-    if (sock_fd == -1){
-	printf("error getting socket: %s", strerror(errno));
-    	return -1;
+    if (sock_fd == -1)
+    {
+        sprintf(error_msg, "error getting socket: %s", strerror(errno));
+        LOGI("%s", error_msg);
+	    printf("error getting socket: %s", error_msg);
+        return -1;
     }
     memset(&user_sockaddr, 0, sizeof(user_sockaddr));
 
@@ -28,8 +35,11 @@ int main(int argc, char* argv[])
     user_sockaddr.nl_groups = MY_GROUP;
 
     retval = bind(sock_fd, (struct sockaddr*)&user_sockaddr, sizeof(user_sockaddr));
-    if(retval < 0){
-        printf("bind failed: %s", strerror(errno));
+    if(retval < 0)
+    {
+        sprintf(error_msg, "bind failed: %s", strerror(errno));
+        LOGI("%s", error_msg);
+        printf("bind failed: %s", error_msg);
         close(sock_fd);
         return -1;
     }
@@ -37,7 +47,9 @@ int main(int argc, char* argv[])
     while (1)
     {
         nl_msghdr = (struct nlmsghdr*) malloc(NLMSG_SPACE(MAX_SIZE));
-	if(!nl_msghdr){
+	    if(!nl_msghdr)
+        {
+            LOGI("%s", "malloc nlmsghdr error!\n");
             printf("malloc nlmsghdr error!\n");
             close(sock_fd);
             return -1;
@@ -46,14 +58,18 @@ int main(int argc, char* argv[])
         iov.iov_base = (void*) nl_msghdr;
         iov.iov_len = NLMSG_SPACE(MAX_SIZE);
 
-	memset(&msghdr, 0, sizeof(msghdr));
+    	memset(&msghdr, 0, sizeof(msghdr));
         //msghdr.msg_name = (void*) &user_sockaddr;
-	//msghdr.msg_namelen = sizeof(user_sockaddr);
-	msghdr.msg_iov = &iov;
-	msghdr.msg_iovlen = 1;
-	recvmsg(sock_fd, &msghdr, 0);
-	printf("%s\n", NLMSG_DATA(nl_msghdr));
+    	//msghdr.msg_namelen = sizeof(user_sockaddr);
+    	msghdr.msg_iov = &iov;
+    	msghdr.msg_iovlen = 1;
+    	recvmsg(sock_fd, &msghdr, 0);
+        kernel_msg = (char*)NLMSG_DATA(nl_msghdr);
+        LOGI("%s", kernel_msg);
+    	printf("%s\n", kernel_msg);
+        
     }
+    free(kernel_msg);
     close(sock_fd);
 
     return 0;
